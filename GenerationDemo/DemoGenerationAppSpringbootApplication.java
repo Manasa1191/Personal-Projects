b4ee34f3d900ada7d00c240a4c6e0ef96a4f9eb6
Manasa <120409509+Manasa1191@users.noreply.github.com>
2023-02-15 19:12:56 -0800

    main method

---
+++ GenerationDemo/DemoGenerationAppSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.generationApp.springboot.demogeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGenerationAppSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGenerationAppSpringbootApplication.class, args);
+	}
+
+}
